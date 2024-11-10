c939961f5c7a83dd059d31bae8968af6be7bc661
fahaddahmed <fahadaahmed8@gmail.com>
2024-11-09 22:08:44 -0500

    Add HTTP Response Functionality

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,20 @@ import java.net.ServerSocket;
 
 fun main() {
     println("Logs")
-    var serverSocket = ServerSocket(4221)
-    serverSocket.reuseAddress = true
-    serverSocket.accept() // Wait for connection from client
-    println("accepted new connection")
+    val serverSocket = ServerSocket(4221).apply {
+        reuseAddress = true // Ensures we don’t run into 'Address already in use' errors
+    }
+    // Accept a client connection
+    val client = serverSocket.accept() // Wait for connection from client.
+    println("Accepted new connection")
+
+    // Handle the client's output
+    val output = client.getOutputStream()
+    output.write("HTTP/1.1 200 OK\r\n\r\n".toByteArray())
+    output.flush()
+    output.close()
+
+    // Close the client and server sockets
+    client.close()
+    serverSocket.close()
 }
