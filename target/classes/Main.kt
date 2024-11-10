3c0b6b0fb998317edbd886ca430d12a1d96f44d8
fahaddahmed <fahadaahmed8@gmail.com>
2024-11-09 21:55:37 -0500

    Your commit message

--- target/classes/Main.kt
+++ target/classes/Main.kt
@@ -2,4 +2,8 @@ import java.net.ServerSocket;
 
 fun main() {
     println("Logs")
+    var serverSocket = ServerSocket(4221)
+    serverSocket.reuseAddress = true
+    serverSocket.accept() // Wait for connection from client.
+    println("accepted new connection")
 }
