179a11dd51470d491c87771c5dc4450588393bd2
fahaddahmed <fahadaahmed8@gmail.com>
2024-11-09 22:13:11 -0500

    Implement URL Path Extraction

--- target/classes/Main.kt
+++ target/classes/Main.kt
@@ -9,13 +9,30 @@ fun main() {
     val client = serverSocket.accept() // Wait for connection from client.
     println("Accepted new connection")
 
-    // Handle the client's output
-    val output = client.getOutputStream()
-    output.write("HTTP/1.1 200 OK\r\n\r\n".toByteArray())
-    output.flush()
-    output.close()
+    // Accept a client connection
+    val client = serverSocket.accept() // Wait for connection from client.
+    println("Accepted new connection")
 
-    // Close the client and server sockets
+    // Handle the client's output & input
+    val input = client.getInputStream()
+    val output = client.getOutputStream()
+
+    input.bufferedReader().use { reader ->
+        val line = reader.readLine()
+        val splitLine = line.split(' ')
+        val path = if (splitLine.size > 1) splitLine[1] else "/"
+
+        if (path == "/") {
+            output.write("HTTP/1.1 200 OK\r\n\r\n".toByteArray())
+        } else {
+            output.write("HTTP/1.1 404 Not Found\r\n\r\n".toByteArray())
+        }
+        output.flush()
+    }
+
+    // Close the output and the client and server sockets
+    output.close()
     client.close()
     serverSocket.close()
 }
+
